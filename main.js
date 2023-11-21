a97684e4e76a37e2786c168b04c52fe62245dcb8
mdrahed2023 <mdrahed708@gmail.com>
2023-11-21 17:33:04 +0600

    hello

---
+++ main.js
@@ -0,0 +1,27 @@
+
+
+let context = document.querySelectorAll(".context");
+
+let menu = document.querySelectorAll(".navigation_bar li");
+
+
+menu.forEach(function (e) {
+
+  e.addEventListener("click", function () {
+    let selectedItem = e.querySelector("a").textContent.toLowerCase();
+
+    context.forEach(function (d) {
+
+      context.forEach(function (params) {
+        params.style.display = 'none';
+      })
+
+      if (selectedItem == 'orders') {
+        document.querySelector(".orders").style.display = 'block';
+      }else if(selectedItem == 'dashboard'){
+        document.querySelector(".dashboard").style.display = 'block';
+      }
+
+    })
+  })
+})
